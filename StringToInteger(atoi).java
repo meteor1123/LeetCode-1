08de65dfccfc037537c725138069f566ab484251
coolthluo <zhengbin.luo@gmail.com>
2015-01-27 15:29:56 -0800

    accepted version

--- StringToInteger(atoi).java
+++ StringToInteger(atoi).java
@@ -4,7 +4,6 @@ Hint: Carefully consider all possible input cases.
 If you want a challenge, please do not see below and ask yourself what are the possible input cases.
 Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
 
-
 */
 
  /*
